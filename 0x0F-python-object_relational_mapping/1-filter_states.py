6d63040b07cc9eee20815baacc0ef57a5dd9bb98
csamiii <chidisamuel16@gmail.com>
2023-03-19 06:15:37 +0000

    lists of N states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" List all states starting with N """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost", usr=agrv[1], passwd=argv[2], db=argv[3], port=3306)
+    db_cur = db.cursor()
+    db_cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    n_rows = db_cur.fetchall()
+    for n in n_rows:
+        print(n)
+    db_cur.close()
+    db.close()
