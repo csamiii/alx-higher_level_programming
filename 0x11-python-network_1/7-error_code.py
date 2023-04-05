48b837f1cfec7936fcd3e91bf73e36d3488b2044
csamiii <chidisamuel16@gmail.com>
2023-04-04 21:50:47 -0700

    debugging

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL, sends a
+Script that takes in a URL, sends a
 request to the URL and displays the body of the
 response
 """
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     if result.status_code >= 400:
         print("Error code: {}".format(result.status_code))
     else:
-        print(result.text)i
+        print(result.text)
