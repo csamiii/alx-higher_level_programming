cd6efc6345c3d73a99d13a1317bb42eb9395a956
csamiii <chidisamuel16@gmail.com>
2023-04-04 19:54:06 -0700

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""A script that fetches https://alx-intranet.hbtn.io/status."""
+"""A script that fetches 
+https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
+    requestUrl = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(requestUrl) as response:
         bodyResponse = response.read()
         print("Body Response:")
         print("\t- type: {}".format(type(bodyResponse)))
