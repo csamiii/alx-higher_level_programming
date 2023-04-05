dcf1eb45ac5c6605d5a05b6b2d25ab7a1c4e45a8
csamiii <chidisamuel16@gmail.com>
2023-04-04 20:10:58 -0700

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         bodyResponse = response.read()
-        print("Body Response:")
+        print("Body response:")
         print("\t- type: {}".format(type(bodyResponse)))
         print("\t- content: {}".format(bodyResponse))
         print("\t- utf8 content: {}".format(bodyResponse.decode("utf-8")))
